edb13ee73284a53e458276838f0283d0a2423460
aguedecc <aguedecc@hotmail.com>
2026-04-15 21:05:28 +0100

    Add files via upload

---
+++ 6051/sessao_14_04_2026/principal2.py
@@ -0,0 +1,11 @@
+while True:
+    # variavel de enrada
+    sensor_detetor = False
+
+    # processamento
+    if sensor_detetor == True:
+        # variavel de saida
+        toca_alarme = True
+    else:
+        #variavel de saida
+        toca_alarme = False
---
+++ 6051/sessao_14_04_2026/principal3or.py
@@ -0,0 +1,11 @@
+#ciclo principal
+while True:
+    # dados de entrada
+    porta = True
+    janela = True
+
+    # processamento
+    if porta or janela:
+        ligar_alarme = True
+    else:
+        ligar_alarme = False
