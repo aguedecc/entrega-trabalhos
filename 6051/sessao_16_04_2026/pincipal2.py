d9efbad327ac23653d3336e403a30d4cdb56f3e7
aguedecc <aguedecc@hotmail.com>
2026-04-16 22:39:22 +0100

    Add files via upload

---
+++ 6051/sessao_16_04_2026/pincipal2.py
@@ -0,0 +1,11 @@
+# ciclo principal
+
+while True:
+    # dados de entrada
+    deposito_de_agua = 2000
+
+    # processamento
+    if deposito_de_agua < 1900:
+        ativa_bomba_de_agua = True
+    else:
+        ativa_bomba_de_agua = False
---
+++ 6051/sessao_16_04_2026/principal4.py
@@ -0,0 +1,14 @@
+# ciclo principal
+ 
+while True:
+    # dados de entrada
+    sen_de_luz1 = 10
+    sen_de_luz2 = 10
+    sen_de_luz3 = 10
+    sen_de_luz4 = 10
+
+    # processamento
+    if sen_de_luz1 <= 10 or sen_de_luz2 <= 10 or sen_de_luz3 <= 10 or sen_de_luz4 <= 10:
+        ativa_luz = True
+    else:
+        activa_luz = False
